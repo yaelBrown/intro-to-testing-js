4de3e64d3a87644d01d09e1a0b5b6f81df9a5ad3
yaelBrown <yaelrbrown@gmail.com>
2019-08-09 20:54:57 -0500

    Setup file to complete during the weekend.

--- code.js
+++ code.js
@@ -2,3 +2,28 @@
 function helloWorld() {
     return "Hello, World!";
 }
+
+// sum function
+function sum(a, b) {
+    return a+b;
+}
+
+// isTrue function
+
+// isFalse function
+
+// notBool function
+
+// addOne function
+
+// isEven function
+
+// isIdentical function
+
+// isEqual function
+
+// or function 
+
+// and function
+
+// concat function
