909996e067f3ede0cd81d17f4fa23d955da4ecca
Yael Brown <yaelrbrown@gmail.com>
2019-08-12 00:01:59 -0500

    Created functions, have not written test for them yet.

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-// helloWorld function 
+// helloWorld function
 function helloWorld() {
     return "Hello, World!";
 }
@@ -9,21 +9,51 @@ function sum(a, b) {
 }
 
 // isTrue function
+function isTrue(bool) {
+    return (bool == true);
+}
 
 // isFalse function
+function isTrue(bool) {
+    return (bool == true);
+}
 
 // notBool function
+function notBool(bool) {
+    return !bool;
+}
 
 // addOne function
+function addOne(a) {
+    return a++;
+}
 
 // isEven function
+function isEven(a) {
+    return (a % 2 == 0);
+}
 
 // isIdentical function
+function isIdentical(a, b) {
+    return (a === b);
+}
 
 // isEqual function
+function isEqual(a,b) {
+    return (a == b);
+}
 
-// or function 
+// or function
+function or(a,b) {
+    return (a || b);
+}
 
 // and function
+function and(a,b) {
+    return (a && b);
+}
 
 // concat function
+function concat(a,b) {
+    return (a+b);
+}
